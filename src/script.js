9d1c7b0a536e90e2c9113a9a2074e32547039c4f
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-09-07 08:44:02 +1000

    Basic Working Hash Function

--- src/script.js
+++ src/script.js
@@ -2,11 +2,15 @@ const form = document.getElementById('login');
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents the form from submitting normally
     const username = document.getElementById('name').value;
-    const key = document.getElementById('key').value;    
-    alert(`Name: ${username}\nAccess Key: ${key}`);
+    const key = document.getElementById('key').value;
+    password = hash(key);
+    alert(`Name: ${username}\nAccess Key: ${key}\nHashed Key: ${password}`);
+
 });
 
-function hash() {
+function hash(key) {
     let password = key.split("").reverse().join("");
     return password
 }
+
+
