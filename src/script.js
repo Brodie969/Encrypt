79ddbba13022f7f752957dceb56601cd17e176d7
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-09-06 14:25:19 +1000

    Fixed more variable names

--- src/script.js
+++ src/script.js
@@ -1,7 +1,11 @@
 const form = document.getElementById('login');
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents the form from submitting normally
-    const username = document.getElementById('username').value;
+    const username = document.getElementById('name').value;
     const key = document.getElementById('key').value;    
     alert(`Name: ${username}\nAccess Key: ${key}`);
 });
+
+function hash() {
+    let password = key;
+}
