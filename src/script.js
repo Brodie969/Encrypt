b2408503ae1c56d67d4bb215ef4a5c1e1be9b9e7
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-09-07 09:16:34 +1000

    Inverse Binary Working

--- src/script.js
+++ src/script.js
@@ -3,17 +3,36 @@ form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents the form from submitting normally
     const username = document.getElementById('name').value;
     const key = document.getElementById('key').value;
-    password = hash(key);
-    alert(`Name: ${username}\nAccess Key: ${key}\nHashed Key: ${password}`);
+    pass = hash(key);
+    alert(`Name: ${username}\nAccess Key: ${key}\nHashed Key: ${pass}`);
 
 });
 
 function hash(key) {
-    let password = ""
+    /* Control Flow: 
+    Key is converted to binary
+    Binary is inverted (0 becomes 1 and 1 becomes 0)
+    Inverse binary is converted back to numbers*/
+    let bin = ""
+    let revBin = ""
+    console.log("Access Key: " + key)
     key = key.split("").reverse().join("");
     for (let i = 0; i < key.length; i++) {
         char = key[i];
-        password = password.concat(char.charCodeAt(0).toString(2));
+        bin = bin.concat(char.charCodeAt(0).toString(2));
     }
-    return password;
+    for (i = 0; i < bin.length; i++) {
+        char = bin[i];
+        if (char == 1) {
+            char = "0";
+        } else if (char == 0) {
+            char = "1";
+        }
+        revBin = revBin.concat(char);
+    }
+    
+    console.log("Binary: " + bin);
+    console.log("Inverted Binary: " + revBin);
+
+    return revBin;
 }
