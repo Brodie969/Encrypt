16267bb17ea6ce488d9e9279b805a7ca915d27d2
Brodie969 <131664136+Brodie969@users.noreply.github.com>
2023-09-10 17:10:11 +1000

    Working Textareas

--- src/app/script.js
+++ src/app/script.js
@@ -1,16 +1,12 @@
-const form = document.getElementById('send');
+const form = document.getElementById('encrypt');
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents the form from submitting normally
-    const subject = document.getElementById('subject').value;
+    const key = document.getElementById('key').value;
     const text = document.getElementById('content').value;
-    alert(`You are about to send a message, as follows: Subject: ${subject}\n\nDescription: ${text}`);
-    $.update(subject, text);
+    $.update();
 });
 
-$.update = function (subject, text) {
-    let messageArea = $("#message");
-    let title = $(`<h4>${subject}</h4>`);
-    let desc = $(`<p>${text}</p>`);
-    messageArea.append(title);
-    messageArea.append(desc);
+$.update = function () {
+    let input = $("#content").val();
+    $("#encryptout").val(input);
 };
